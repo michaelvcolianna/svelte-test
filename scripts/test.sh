38d7fa5aaf2fcf45e33ef34c63ab7b4d0a4417bd
MVC <michael.colianna@me.com>
2024-03-05 10:28:08 -0500

    boom?

---
+++ scripts/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="${SCRIPT_FOLDER}/.."
+
+if [ ! -d "${WEBSITE_ROOT}/_cache" ]; then
+    echo "${WEBSITE_ROOT}/_cache does not exist, creating"
+    mkdir -p "_cache"
+fi
+
+cd _cache
+
+for i in {1..25};
+    do dd if=/dev/urandom bs=1 count=1 of=file$i;
+done
