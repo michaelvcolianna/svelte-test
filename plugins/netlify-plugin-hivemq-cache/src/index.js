fc1d100c4b50647404a1dd478e073675c681c9f6
MVC <michael.colianna@me.com>
2024-03-08 14:11:25 -0500

    Remove it all except build folder for deploy

--- plugins/netlify-plugin-hivemq-cache/src/index.js
+++ plugins/netlify-plugin-hivemq-cache/src/index.js
@@ -1,4 +1,4 @@
-import { existsSync, readdirSync, rmSync } from 'node:fs'
+import { readdirSync, rmSync } from 'node:fs'
 
 const ROOT_PATH = new URL('../../../', import.meta.url).pathname
 const CACHE_DIR = `${ROOT_PATH}_cache`
@@ -14,9 +14,10 @@ export const onPostBuild = async function ({
 }) {
   await cache.save(CACHE_DIR)
 
-  // if (existsSync(CACHE_DIR)) {
-  //   rmSync(CACHE_DIR, { recursive: true, force: true })
-  // }
+  const ls = readdirSync(ROOT_PATH)
+  ls.filter((entry) => entry !== 'build').forEach(entry => {
+    rmSync(`${ROOT_PATH}${entry}`, { recursive: true, force: true })
+  })
 
   console.log('HiveMQ cache plugin post build:', readdirSync(ROOT_PATH))
 }
