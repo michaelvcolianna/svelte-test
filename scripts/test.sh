a9c5beda5f90e882f74a10764f93ae14c6436e69
MVC <michael.colianna@me.com>
2024-03-08 14:41:24 -0500

    Actually fixes the cache script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="${SCRIPT_FOLDER}/.."
+if [ ! -d ../_cache ]
+then
+    echo "Cache folder does not exist, creating"
+    mkdir -p ../_cache
 
-if [ ! -d "${PROJECT_ROOT}/_cache" ]; then
-    echo "${PROJECT_ROOT}/_cache does not exist, creating"
-    mkdir -p "_cache"
+    cd ../_cache
+
+    for i in {1..25};
+        do dd if=/dev/urandom bs=1 count=1 of=file$i;
+    done
+else
+    echo "Cache folder exists, continuing"
 fi
-
-cd _cache
-
-for i in {1..25};
-    do dd if=/dev/urandom bs=1 count=1 of=file$i;
-done
