502bbbdb4987d43003757660094b763fe8da4d9f
MVC <michael.colianna@me.com>
2024-03-08 14:55:56 -0500

    this should work

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-echo "test.sh"
-pwd
+if [ ! -d "_cache" ]
+then
+    echo "Cache folder does not exist, creating"
+    mkdir -p "_cache"
 
-# if [ ! -d ../_cache ]
-# then
-#     echo "Cache folder does not exist, creating"
-#     mkdir -p ../_cache
+    cd "_cache"
 
-#     cd ../_cache
-
-#     for i in {1..25};
-#         do dd if=/dev/urandom bs=1 count=1 of=file$i;
-#     done
-# else
-#     echo "Cache folder exists, continuing"
-# fi
+    for i in {1..25};
+        do dd if=/dev/urandom bs=1 count=1 of=file$i;
+    done
+else
+    echo "Cache folder exists, continuing"
+fi
