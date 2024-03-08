6ba6cecd5060dd8254be43c967768b6be6536525
MVC <michael.colianna@me.com>
2024-03-08 13:54:24 -0500

    Ok here we go

--- plugins/netlify-plugin-hivemq-cache/src/index.js
+++ plugins/netlify-plugin-hivemq-cache/src/index.js
@@ -1,8 +1,8 @@
 import { existsSync } from 'node:fs'
 
-const ROOT_PATH = new URL('../../../../', import.meta.url).pathname
-const CONFIG_FILE = `${ROOT_PATH}/svelte.config.js`
-const TEST_FILE = `${ROOT_PATH}/scripts/test.sh`
+const ROOT_PATH = new URL('../../../', import.meta.url).pathname
+const CONFIG_FILE = `${ROOT_PATH}svelte.config.js`
+const TEST_FILE = `${ROOT_PATH}scripts/test.sh`
 
 export const onPreBuild = async function ({
   utils: { cache },
@@ -13,7 +13,7 @@ export const onPreBuild = async function ({
   //   build.failBuild('Error message', { error })
   // }
 
-  console.log(`Root path is ${ROOT_PATH}, config file is ${CONFIG_FILE}`)
+  console.log(`Root path is ${ROOT_PATH}, config file is ${CONFIG_FILE}, does config file exist?`, existsSync(CONFIG_FILE))
 }
 
 export const onPostBuild = async function ({
@@ -25,5 +25,5 @@ export const onPostBuild = async function ({
   //   build.failBuild('Error message', { error })
   // }
 
-  console.log(`Test script is ${TEST_FILE}`)
+  console.log(`Test script is ${TEST_FILE}, does test script exist?`, existsSync(TEST_FILE))
 }
