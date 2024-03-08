b06282bce81d9d143f7abf4106a67663b81df50e
MVC <michael.colianna@me.com>
2024-03-08 14:21:22 -0500

    Testing

--- plugins/netlify-plugin-hivemq-cache/src/index.js
+++ plugins/netlify-plugin-hivemq-cache/src/index.js
@@ -1,23 +1,38 @@
-import { readdirSync, rmSync } from 'node:fs'
+import { existsSync, readdirSync, rmSync } from 'node:fs'
 
 const ROOT_PATH = new URL('../../../', import.meta.url).pathname
-const CACHE_DIR = `${ROOT_PATH}_cache`
+const CACHE_PATH = `${ROOT_PATH}_cache`
 
 export const onPreBuild = async function ({
+  constants: { CACHE_DIR },
   utils: { cache },
 }) {
-  await cache.restore(CACHE_DIR)
+  console.log(`HiveMQ cache plugin pre build:`)
+
+  console.log(`-- Is ${CACHE_PATH} in Netlify cache?`, existsSync(`${CACHE_DIR}/_cache`))
+  
+  await cache.restore(CACHE_PATH)
+
+  console.log(`-- ${CACHE_PATH} exists?`, existsSync(CACHE_PATH))
 }
 
 export const onPostBuild = async function ({
+  constants: { CACHE_DIR },
   utils: { cache },
 }) {
-  await cache.save(CACHE_DIR)
+  console.log(`HiveMQ cache plugin post build:`)
 
+  await cache.save(CACHE_PATH)
+  
   const ls = readdirSync(ROOT_PATH)
-  ls.filter((entry) => entry !== 'build').forEach(entry => {
-    rmSync(`${ROOT_PATH}${entry}`, { recursive: true, force: true })
+  ls.filter((entry) => entry !== 'build').forEach((entry) => {
+    const ENTRY_PATH = `${ROOT_PATH}${entry}`
+    console.log(`-- Removing: ${ENTRY_PATH}`)
+    
+    rmSync(ENTRY_PATH, { recursive: true, force: true })
   })
+  
+  console.log('-- Directory contents:', readdirSync(ROOT_PATH))
 
-  console.log('HiveMQ cache plugin post build:', readdirSync(ROOT_PATH))
+  console.log(`-- Is ${CACHE_PATH} in Netlify cache?`, existsSync(`${CACHE_DIR}/_cache`))
 }
