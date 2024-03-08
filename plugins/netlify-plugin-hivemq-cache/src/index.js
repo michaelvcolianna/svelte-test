c9e562849120b6110fbc9267907458094e548781
MVC <michael.colianna@me.com>
2024-03-08 14:05:35 -0500

    Get list of files

--- plugins/netlify-plugin-hivemq-cache/src/index.js
+++ plugins/netlify-plugin-hivemq-cache/src/index.js
@@ -1,4 +1,4 @@
-// import { existsSync } from 'node:fs'
+import { existsSync, readdirSync, rmSync } from 'node:fs'
 
 const ROOT_PATH = new URL('../../../', import.meta.url).pathname
 const CACHE_DIR = `${ROOT_PATH}_cache`
@@ -6,15 +6,17 @@ const CACHE_DIR = `${ROOT_PATH}_cache`
 export const onPreBuild = async function ({
   utils: { cache },
 }) {
-  const restoreCache = await cache.restore(CACHE_DIR)
-
-  console.log(`HiveMQ cache pre-build: ${CACHE_DIR}`, restoreCache)
+  await cache.restore(CACHE_DIR)
 }
 
 export const onPostBuild = async function ({
   utils: { cache },
 }) {
-  const saveCache = await cache.save(CACHE_DIR)
+  await cache.save(CACHE_DIR)
 
-  console.log(`HiveMQ cache post-build: ${CACHE_DIR}`, saveCache)
+  // if (existsSync(CACHE_DIR)) {
+  //   rmSync(CACHE_DIR, { recursive: true, force: true })
+  // }
+
+  console.log('HiveMQ cache plugin post build:', readdirSync(ROOT_PATH))
 }
