bcba44078e60b8844c1d27ba52cb39fda351db1f
MVC <michael.colianna@me.com>
2024-03-20 10:07:28 -0400

    Testing if this is failing too

--- plugins/netlify-plugin-hivemq-cache/src/index.js
+++ plugins/netlify-plugin-hivemq-cache/src/index.js
@@ -1,36 +1,15 @@
-import { existsSync, readdirSync, rmSync } from 'node:fs'
-
-const ROOT_PATH = new URL('../../../', import.meta.url).pathname
-const CACHE_PATH = `${ROOT_PATH}_cache`
-
 export const onPreBuild = async function ({
   utils: { cache },
 }) {
-  console.log(`HiveMQ cache plugin pre build:`)
+  await cache.restore('./_cache')
 
-  console.log(`-- Pre-restore: ${CACHE_PATH} exists?`, existsSync(CACHE_PATH))
-  
-  await cache.restore('_cache')
-
-  console.log(`-- Post-restore: ${CACHE_PATH} exists?`, existsSync(CACHE_PATH))
+  console.log('Cache restore:', await cache.list())
 }
 
 export const onPostBuild = async function ({
   utils: { cache },
 }) {
-  console.log(`HiveMQ cache plugin post build:`)
-
-  await cache.save('_cache')
-
-  console.log(`-- Pre-cleanup: ${CACHE_PATH} exists?`, existsSync(CACHE_PATH))
-
-  const ls = readdirSync(ROOT_PATH)
-  ls.filter((entry) => entry !== 'build').forEach((entry) => {
-    const ENTRY_PATH = `${ROOT_PATH}${entry}`
-    console.log(`-- Removing: ${ENTRY_PATH}`)
-    
-    rmSync(ENTRY_PATH, { recursive: true, force: true })
-  })
+  await cache.save('./_cache')
   
-  console.log(`-- Post-cleanup: ${CACHE_PATH} exists?`, existsSync(CACHE_PATH))
+  console.log('Cache save:', await cache.list())
 }
